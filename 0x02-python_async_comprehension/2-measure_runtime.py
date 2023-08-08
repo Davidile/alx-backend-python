4b56109829588009bdea0f9578626e82442f2301
david <mugomadavid24@gmail.com>
2023-08-08 12:50:17 -0700

    updated changes

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
-"""Using `async` comprehensions instead of loops."""
+"""Measure the time taken by an asynchronous function to complete.
+
+Execute `async_comprehension` four times in parallel and measure
+the total time taken to run.
+"""
 import asyncio
-from typing import List
+import time
 
-async_generator = __import__('0-async_generator').async_generator
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def async_comprehension() -> List[float]:
-    """Build a list using async comprehension."""
-    return [i async for i in async_generator()]
+async def measure_runtime() -> float:
+    """Make 4 concurrent calls of the `async_comprehension` coroutine.
+
+    Measure and return the total time taken to run the coroutine in
+    parallel.
+    Return:
+        float: total time taken.
+    """
+    start = time.perf_counter()
+    coros = [async_comprehension() for _ in range(4)]
+    await asyncio.gather(*coros)
+    end = time.perf_counter() - start
+    return end
